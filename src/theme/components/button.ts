f6d831b5a13cc126637b0b5bd98981112b188fc8
Inna Iazovskaia <inna.0961@mail.ru>
2023-03-10 19:24:51 +0100

    Create styles formButton in button.ts

---
+++ src/theme/components/button.ts
@@ -0,0 +1,21 @@
+export const formButton = {
+  formButton: {
+    backgroundColor: "brand.800",
+    color: "brand.50",
+    _hover: {
+      backgroundColor: "brand.50",
+      color: "brand.800",
+      border: "1px solid",
+      borderColor: "brand.800",
+    },
+    _disabled: {
+      backgroundColor: "brand.400",
+      color: "brand.50",
+      _hover: {
+        border: "1px solid",
+        borderColor: "brand.400",
+        color: "brand.400",
+      },
+    },
+  },
+};

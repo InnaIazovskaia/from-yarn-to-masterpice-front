cdd4059e486c8f354cd84aa2e9f5060909945f13
Inna Iazovskaia <inna.0961@mail.ru>
2023-03-11 17:11:28 +0100

    Create custom formInput styles in formComponents.ts

---
+++ src/theme/components/formComponents.ts
@@ -0,0 +1,30 @@
+export const formButton = {
+  backgroundColor: "brand.800",
+  color: "brand.50",
+  _hover: {
+    backgroundColor: "brand.50",
+    color: "brand.800",
+    border: "1px solid",
+    borderColor: "brand.800",
+  },
+  _disabled: {
+    backgroundColor: "brand.400",
+    color: "brand.50",
+    _hover: {
+      border: "1px solid",
+      borderColor: "brand.400",
+      color: "brand.400",
+    },
+  },
+};
+
+export const formInput = {
+  field: {
+    border: "1px solid",
+    borderColor: "brand.400",
+    _focus: {
+      border: "2px",
+      borderColor: "brand.800",
+    },
+  },
+};

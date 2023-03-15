3fd12ec469141df56d05a3e17f0bb3ed9c8de621
Inna Iazovskaia <inna.0961@mail.ru>
2023-03-15 14:42:45 +0100

    Remove Heading and change isDisabled in LoginForm

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,15 +1,13 @@
-import React from "react";
 import {
   Box,
   FormControl,
   FormErrorMessage,
   FormLabel,
-  Heading,
   Input,
   VStack,
 } from "@chakra-ui/react";
 import { Formik, Field, Form } from "formik";
-import { type UserCredentials } from "../../types";
+import { UserCredentials } from "../../types";
 import {
   validatePassword,
   validateUsername,
@@ -22,24 +20,19 @@ const LoginForm = (): JSX.Element => {
     password: "",
   };
 
-  let isDisabled: boolean;
-
   return (
     <Box p={[5, 10, 15]} rounded="md" w={["full", "md", "lg"]} mx="auto">
       <Formik
         initialValues={{
           ...initialFormData,
         }}
-        onSubmit={(values, { resetForm }) => {
+        onSubmit={async (values, { resetForm }) => {
           resetForm();
         }}
       >
         {({ errors, touched, values }) => (
           <Form>
-            <VStack spacing={5} align="flex-start" w="full">
-              <Heading w="full" textAlign="center">
-                Login
-              </Heading>
+            <VStack spacing={7} align="flex-start" w="full">
               <FormControl
                 isInvalid={Boolean(errors.username) && touched.username}
               >
@@ -58,6 +51,7 @@ const LoginForm = (): JSX.Element => {
               >
                 <FormLabel htmlFor="password">Password</FormLabel>
                 <Field
+                  mb={5}
                   as={Input}
                   id="password"
                   name="password"
@@ -69,12 +63,10 @@ const LoginForm = (): JSX.Element => {
               </FormControl>
               <FormButton
                 text="Login"
-                isdDisabled={
-                  (values.password === "" ||
-                    values.username === "" ||
-                    values.password.length < 8) &&
-                  errors.password !== "" &&
-                  errors.username !== ""
+                isDisabled={
+                  values.password === "" ||
+                  values.username === "" ||
+                  values.password.length < 8
                 }
               />
             </VStack>

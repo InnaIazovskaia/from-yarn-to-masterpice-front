13500c245142847ee6850fb58603af05a6ca2b77
Inna Iazovskaia <inna.0961@mail.ru>
2023-03-17 20:04:45 +0100

    Create variants navLink and iconLink

---
+++ src/theme/components/navComponents.ts
@@ -0,0 +1,11 @@
+export const navLink = {
+  fontWeight: "600",
+  fontSize: "1.2rem",
+};
+
+export const iconLink = {
+  size: "md",
+  colorScheme: "white",
+  color: "brand.900",
+  fontWeight: "normal",
+};

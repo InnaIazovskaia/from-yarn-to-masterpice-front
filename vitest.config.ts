608b66824b1587530ce423f788fcc1399eb4f792
Inna Iazovskaia <inna.0961@mail.ru>
2023-03-20 20:13:15 +0100

    Change test provider to istanbul and add coverage configurations

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,9 +5,21 @@ export default defineConfig({
     globals: true,
     setupFiles: "setupTests.ts",
     environment: "jsdom",
+    include: ["src/**/*.test.ts", "src/**/*.test.tsx"],
     coverage: {
-      provider: "c8",
+      provider: "istanbul",
       reportsDirectory: "coverage",
+      reporter: ["lcov", "html", "text"],
+      include: ["src/**/*"],
+      exclude: [
+        "src/**/*.test.ts",
+        "src/**/*.test.tsx",
+        "src/vite-env.d.ts",
+        "src/main.tsx",
+        "src/theme/**/*.*",
+        "src/stores/**/*.*",
+      ],
+      all: true,
     },
   },
 });

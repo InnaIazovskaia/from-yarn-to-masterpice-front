ba7d5efb67d6660b5d765e7cfc49acfc885025d9
Inna Iazovskaia <inna.0961@mail.ru>
2023-03-20 20:51:38 +0100

    Create tests for Footer component

---
+++ src/components/Footer/Footer.test.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import Footer from "./Footer";
+
+describe("Given a Footer component", () => {
+  describe("When it's rendered", () => {
+    it("Should show text 'Made by Inna Iazovskaia'", () => {
+      const expectedText = "Made by Inna Iazovskaia";
+
+      render(<Footer />);
+
+      const text = screen.getByText(expectedText);
+
+      expect(text).toBeInTheDocument();
+    });
+
+    it("Should show text 'All questions about the master classes and patterns you can sent to email'", () => {
+      const expectedText =
+        "All questions about the master classes and patterns you can sent to email";
+
+      render(<Footer />);
+
+      const text = screen.getByText(expectedText);
+
+      expect(text).toBeInTheDocument();
+    });
+
+    it("Should show", () => {
+      render(<Footer />);
+
+      const link = screen.getByRole("link");
+
+      expect(link).toBeInTheDocument();
+    });
+  });
+});

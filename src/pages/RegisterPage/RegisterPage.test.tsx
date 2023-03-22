3bc7913ae59e7b82b7f9d87fb6c1f4b0d86fb9f8
Inna Iazovskaia <inna.0961@mail.ru>
2023-03-22 15:28:53 +0100

    Create tests for RegisterPage

---
+++ src/pages/RegisterPage/RegisterPage.test.tsx
@@ -0,0 +1,54 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import { BrowserRouter, Navigate } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+import { describe, expect, it } from "vitest";
+import RegisterPage from "./RegisterPage";
+
+describe("Given a RegisterPage component", () => {
+  describe("When it's rendered", () => {
+    it("Should show heading 'REGISTER'", () => {
+      const headingText = "REGISTER";
+
+      render(
+        <BrowserRouter>
+          <RegisterPage />
+        </BrowserRouter>
+      );
+
+      const heading = screen.getByRole("heading", {
+        level: 1,
+        name: headingText,
+      });
+
+      expect(heading).toBeInTheDocument();
+    });
+
+    it("Should show link 'Create an account'", () => {
+      const linkText = "Log in";
+
+      render(
+        <BrowserRouter>
+          <RegisterPage />
+        </BrowserRouter>
+      );
+
+      const link = screen.getByRole("link", { name: linkText });
+
+      expect(link).toBeInTheDocument();
+    });
+
+    it("Should show text 'Already a member?'", () => {
+      const expectedText = "Already a member?";
+
+      render(
+        <BrowserRouter>
+          <RegisterPage />
+        </BrowserRouter>
+      );
+
+      const text = screen.getByText(expectedText);
+
+      expect(text).toBeInTheDocument();
+    });
+  });
+});

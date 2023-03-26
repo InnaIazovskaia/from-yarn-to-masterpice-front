7479f6ab791dc7667a43de686bd8cd94e7c8ba81
Inna Iazovskaia <inna.0961@mail.ru>
2023-03-26 14:37:05 +0200

    Delete not used imports from RegisterPage.tests.ts

--- src/pages/RegisterPage/RegisterPage.test.tsx
+++ src/pages/RegisterPage/RegisterPage.test.tsx
@@ -1,6 +1,5 @@
-import { render, screen, waitFor } from "@testing-library/react";
-import { BrowserRouter, Navigate } from "react-router-dom";
-import userEvent from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import { describe, expect, it } from "vitest";
 import RegisterPage from "./RegisterPage";
 

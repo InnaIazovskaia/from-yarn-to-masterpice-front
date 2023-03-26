6f40c260d7231b54dd76ab4a6008fdd96942ff2c
Inna Iazovskaia <inna.0961@mail.ru>
2023-03-26 15:48:06 +0200

    Add userRegister endpoint

--- src/hooks/useUser/userEndpoints.ts
+++ src/hooks/useUser/userEndpoints.ts
@@ -1,11 +1,13 @@
 interface UserEndpoints {
   userEndpoint: string;
   loginEndpoint: string;
+  registerEndpoint: string;
 }
 
 const userEndpoints: UserEndpoints = {
   userEndpoint: "/user",
   loginEndpoint: "/login",
+  registerEndpoint: "/register",
 };
 
 export default userEndpoints;
